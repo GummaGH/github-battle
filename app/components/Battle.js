201717b2b33523aef213c0dc6f23cb5921f0cfe9
gummagh <gummagh@github.com>
2017-11-20 22:20:25 +0000

    Finished Results screen. Extracted PlayerPreview to own component for reuse in Battle and Results. Feature complete Github Battle App.

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,33 +1,7 @@
 var React = require('react');
 var PropTypes = require('prop-types');
 var Link = require('react-router-dom').Link;
-
-function PlayerPreview (props) {
-  return (
-    <div>
-      <div className='column'>
-        <img
-          className='avatar'
-          src={props.avatar}
-          alt={'Avatar for ' + props.username}
-        />
-        <h2 className='username'>@{props.username}</h2>
-      </div>
-      <button
-        className='reset'
-        onClick={props.onReset.bind(null, props.id)}>
-          Reset
-        </button>
-    </div>
-  )
-}
-
-PlayerPreview.propTypes = {
-  avatar: PropTypes.string.isRequired,
-  username: PropTypes.string.isRequired,
-  id: PropTypes.string.isRequired,
-  onReset: PropTypes.func.isRequired
-}
+var PlayerPreview = require('./PlayerPreview');
 
 class PlayerInput extends React.Component {
   constructor(props) {
@@ -143,11 +117,15 @@ class Battle extends React.Component {
 
           {playerOneImage !== null &&
             <PlayerPreview
-              id='playerOne'
               avatar={playerOneImage}
               username={playerOneName}
-              onReset={this.handleReset}
-            />}
+            >
+              <button
+                className='reset'
+                onClick={this.handleReset.bind(null, 'playerOne')}>
+                  Reset
+              </button>
+            </PlayerPreview>}
 
           {!playerTwoName &&
             <PlayerInput
@@ -158,11 +136,15 @@ class Battle extends React.Component {
 
           {playerTwoImage !== null &&
               <PlayerPreview
-                id='playerTwo'
                 avatar={playerTwoImage}
                 username={playerTwoName}
-                onReset={this.handleReset}
-              />}
+              >
+                <button
+                  className='reset'
+                  onClick={this.handleReset.bind(null, 'playerTwo')}>
+                    Reset
+                </button>
+              </PlayerPreview>}
         </div>
 
         {playerOneImage && playerTwoImage &&
